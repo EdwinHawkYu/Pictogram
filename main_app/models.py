3cdd900a403d4297c946bae1f0cb96fe3c71adcb
Wasoak <Wasimokadia@gmail.com>
2022-09-18 12:05:29 -0400

    add models

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,33 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+class Post(models.Model):
+   description = models.TextField(max_length=100)
+   user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+   def __str__(self):
+        return self.name
+
+
+class Like(models.Model):
+    likes = models.ManyToManyField(User, name='post_like')
+
+    def number_of_likes(self):
+        return self.likes.count()
+
+class Comment(models.Model):
+    description = models.TextField(max_length=100)
+
+    post = models.ForeignKey(Post,on_delete=models.CASCADE, name='comments')
+
+    def __str__(self):
+        return 'Comment {} by {}'.format(self.body, self.name)
+
+class Photo(models.Model):
+  url = models.CharField(max_length=200)
+  post = models.ForeignKey(Post, on_delete=models.CASCADE)
+
+  def __str__(self):
+    return f"Photo for post_id: {self.post_id} @{self.url}"
